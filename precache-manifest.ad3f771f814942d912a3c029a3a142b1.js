1c048ff67f802506bff29c6ef5972693d3a555d6
Kiara <kiara.oliver318@gmail.com>
2020-11-21 17:00:00 -0500

    Updates

---
+++ precache-manifest.ad3f771f814942d912a3c029a3a142b1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cbbf0c3f8aa507e6110",
+    "url": "/TeaMateReact/static/css/main.e6a3fdb7.chunk.css"
+  },
+  {
+    "revision": "8cbbf0c3f8aa507e6110",
+    "url": "/TeaMateReact/static/js/main.f0752a1e.chunk.js"
+  },
+  {
+    "revision": "676d7201d9aa494b95fa",
+    "url": "/TeaMateReact/static/js/runtime~main.74e0834f.js"
+  },
+  {
+    "revision": "07d04033340b43e0d0c9",
+    "url": "/TeaMateReact/static/js/2.0a51c6a0.chunk.js"
+  },
+  {
+    "revision": "b95dd7ed0d02c8e5c9d3af391dfb4b8d",
+    "url": "/TeaMateReact/static/media/AvenirNext-Italic.b95dd7ed.ttf"
+  },
+  {
+    "revision": "d32a9b02c6322f509ab5fc479409d5fc",
+    "url": "/TeaMateReact/static/media/AvenirNext-Bold.d32a9b02.ttf"
+  },
+  {
+    "revision": "5e8d8aa679d32fa464cf8347672fb583",
+    "url": "/TeaMateReact/static/media/AvenirNext-DemiBold.5e8d8aa6.ttf"
+  },
+  {
+    "revision": "c7d4eaa279bf8ae8fbf2b91c4d7e38a9",
+    "url": "/TeaMateReact/static/media/AvenirNext-Medium.c7d4eaa2.ttf"
+  },
+  {
+    "revision": "3c041f665736d0f1040880ec0d0552f2",
+    "url": "/TeaMateReact/static/media/AvenirNext-Regular.3c041f66.ttf"
+  },
+  {
+    "revision": "fc079315cf46d3b561e8ae43dd7ada63",
+    "url": "/TeaMateReact/static/media/DetailsScreen_elPicture_21119.fc079315.jpg"
+  },
+  {
+    "revision": "b21d6fe9bc964e04eea1922019e23fd2",
+    "url": "/TeaMateReact/static/media/AdventuresTitle_elHeaderImage_846627.b21d6fe9.jpg"
+  },
+  {
+    "revision": "9fe468cd511d377fb722afba93165234",
+    "url": "/TeaMateReact/static/media/AdventureItem_elPicture_333009.9fe468cd.jpg"
+  },
+  {
+    "revision": "eeedcb3f1b6447026f11799e36d3744c",
+    "url": "/TeaMateReact/static/media/LoginScreen_elAca06b1e9c6a60d6091f38b5bc584bee_828553.eeedcb3f.jpg"
+  },
+  {
+    "revision": "904d9edc4c5d6217438977ed415f47d5",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row0_picture.904d9edc.png"
+  },
+  {
+    "revision": "253ff3442752f7d7fb1ad56edcd01172",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row1_picture.253ff344.png"
+  },
+  {
+    "revision": "bd47753a40f3bc9d4d613bb064ebf421",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row2_picture.bd47753a.png"
+  },
+  {
+    "revision": "601d650908b3801e8d2776b871f3eee9",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row3_picture.601d6509.png"
+  },
+  {
+    "revision": "e5b578f3d53913415becb24392ac2701",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row4_picture.e5b578f3.png"
+  },
+  {
+    "revision": "601d650908b3801e8d2776b871f3eee9",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row5_picture.601d6509.png"
+  },
+  {
+    "revision": "c81fe9859d1c0cc194936809185e3f45",
+    "url": "/TeaMateReact/static/media/datasheet_listData1_row6_picture.c81fe985.png"
+  },
+  {
+    "revision": "0e3f6cee4001eb51537b549d180b9bc2",
+    "url": "/TeaMateReact/index.html"
+  }
+];
